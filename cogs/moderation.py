5ed788c3f2857bdc7dd9a9e7d66bc1756eb8d164
zhyted <vurvfx@gmail.com>
2024-09-14 00:13:29 +0100

    last fix hopefully

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,7 +8,7 @@ class Moderation(commands.Cog):
         self.bot = bot
     
     
-    @commands.command(description="responds with your roles (slash commands test)")
+    @app_commands.command(description="responds with your roles (slash commands test)")
     async def getroles(self, interaction: discord.Interaction):
         await interaction.response.send_message( str(interaction.user.roles) , ephemeral= True)
     
