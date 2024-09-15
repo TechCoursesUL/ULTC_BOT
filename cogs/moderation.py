9565aa2625acab2a32c405e5e172ff8e751cae61
zhyted <vurvfx@gmail.com>
2024-09-15 21:51:49 +0100

    test

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -48,11 +48,11 @@ class Moderation(commands.Cog):
             try:
                 await f(ctx, *args, **kwargs)
             except Exception as e:
-                await args[0].response.send_message(f"(decorator) Command Failed- {e}")  
+                await args[1].response.send_message(f"(decorator) Command Failed- {e}")  
                 
         decorator.__name__ = f.__name__
         sig = inspect.signature(f)
-        decorator.__signature__ = sig.replace(parameters=tuple(sig.parameters.values()))
+        decorator.__signature__ = sig.replace(parameters=tuple(sig.parameters.values())[0:])
         return decorator
     
 
