619a5285880113a9df43eb5fac4ffc2c9d50bbac
zhyted <vurvfx@gmail.com>
2024-09-16 15:39:48 +0100

    attempted fix

--- db.py
+++ db.py
@@ -20,11 +20,11 @@ class ULTCDB:
     def __init__(self) -> None:
         self.app = firebase_admin.initialize_app( credential=firebase_admin.credentials.Certificate(json.loads(os.getenv("DBAUTH"))), options={'databaseURL' :'https://ultcdb-default-rtdb.europe-west1.firebasedatabase.app/'} )
         
-    async def GetBannedUsers(self) -> json:
+    def GetBannedUsers(self):
         print(f"getting banned users from db...")
         
         ref = db.reference("server/users/bannedusers")
-        return await ref.get()
+        return ref.get()
     
     async def SetBannedUsers(self, bannedUsers: json):
         ref = db.reference("server/users/bannedusers")
